fae9c4e7951973e54d0b159d63bd5ee12a5357e9
tenthirtyone <alexsherbuck@gmail.com>
2020-12-02 13:02:45 -0500

    rinkeby deployment

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,9 @@
+require("dotenv").config();
 const path = require("path");
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = process.env["RINKEBY_MNEMONIC"];
+var rinkebyKey = process.env["RINKEBY_KEY"];
+var tokenKey = process.env["INFURA_KEY"];
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -6,7 +11,19 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     develop: {
-      port: 8545
-    }
-  }
+      port: 8545,
+    },
+    rinkeby: {
+      host: "localhost",
+      provider: function () {
+        return new HDWalletProvider(
+          rinkebyKey,
+          "https://rinkeby.infura.io/v3/" + tokenKey
+        );
+      },
+      network_id: 4,
+      gas: 6700000,
+      gasPrice: 30000000000,
+    },
+  },
 };
