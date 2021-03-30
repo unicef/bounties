dd75cb1616da518d7870bc6c2ef01d635efc5193
tenthirtyone <alexsherbuck@gmail.com>
2021-03-30 16:05:04 -0400

    make fulfillment

---
+++ client/src/actions/makeFulfillment.js
@@ -0,0 +1,7 @@
+export default async (account, contract, bountyId, data) => {
+  const boostAddress = "0x4f1690e9b1576a997e40334674d3f14e966bb7bb";
+  const zeroAddress = "0x0000000000000000000000000000000000000000";
+  return await contract.methods
+    .fulfill(account, [account], bountyId, data)
+    .send({ from: account });
+};
